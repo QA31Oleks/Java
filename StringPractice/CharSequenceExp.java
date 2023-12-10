ed486778b566fdd3ab003710f72e644518f7c812
Oleksandr <136840623+QA31Oleks@users.noreply.github.com>
2023-12-10 13:49:20 +0200

    Delete StringPractice directory

--- StringPractice/CharSequenceExp.java
+++
@@ -1,8 +0,0 @@
-package org.StringPractice;
-
-public class CharSequenceExp {
-    public static void main(String[] args) {
-        String s = "Привіт, мужик!";
-        System.out.println(s.charAt(0));
-    }
-}
