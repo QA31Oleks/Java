f089a81731ee85e8a5a251ee0a98d74cbe42fcd2
QA31Oleks <aleksandr.tsupko@gmail.com>
2023-12-10 14:28:07 +0200

    Lists edit

--- src/main/java/org/Lists/CreateEditRemove.java
+++ src/main/java/org/Lists/CreateEditRemove.java
@@ -16,11 +16,15 @@ public class CreateEditRemove {
         // output [3, 1, 2]
         l1.add(0, 3);
 
+        // Now remove the current object from the updated
+        // List
+        l1.remove(0);
+
         // Print the elements inside the object
         System.out.println(l1);
 
         // Creating another object of the List
-        List<Integer> l2 = new ArrayList<Integer>();
+        List<Integer> l2 = new ArrayList<>();
 
         // Again adding elements to object of List interface
         // Custom inputs
@@ -29,6 +33,7 @@ public class CreateEditRemove {
         l2.add(3);
 
         // Will add list l2 from 1 index
+        // output [1, 1, 2, 3, 2]
         l1.addAll(1, l2);
 
         System.out.println(l1);
@@ -56,5 +61,13 @@ public class CreateEditRemove {
         // the element is not found
         // output -1
         System.out.println(l1.indexOf(10));
+
+        // Accessing elements using get() method
+        String first = s1.get(0);
+
+        // Checking if element is present
+        // using contains() method
+        // output False
+        boolean isPresent = s1.contains("Geeks");
     }
 }
