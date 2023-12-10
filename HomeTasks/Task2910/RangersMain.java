10c93cb7360e7385498f692de546bbcc11b9e3b9
Oleksandr <136840623+QA31Oleks@users.noreply.github.com>
2023-12-10 13:49:03 +0200

    Delete HomeTasks directory

--- HomeTasks/Task2910/RangersMain.java
+++
@@ -1,9 +0,0 @@
-package org.HomeTasks.Task2910;
-
-public class RangersMain {
-    public static void main(String[] args) {
-        RangesClass rangesClass = new RangesClass();
-        System.out.println(rangesClass.getArray1().get(0));
-        System.out.println(rangesClass.getArray1().get(rangesClass.getArray1().size()-1));
-    }
-}
