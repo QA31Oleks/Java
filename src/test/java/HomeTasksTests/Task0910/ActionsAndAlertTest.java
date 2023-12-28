b6e547779e5333b2e6ea0c3dce9ffbe3b58da2ae
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2023-12-28 09:36:40 +0200

    Add DisplayNumberFromList in Practice

--- src/test/java/HomeTasksTests/Task0910/ActionsAndAlertTest.java
+++ src/test/java/HomeTasksTests/Task0910/ActionsAndAlertTest.java
@@ -1,13 +1,13 @@
 package HomeTasksTests.Task0910;
 
-//import org.example.Task0910.ActionsAndAlert;
+import org.HomeTasks.Task0910.ActionsAndAlert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/*public class ActionsAndAlertTest {
+public class ActionsAndAlertTest {
     // метод перевіряє наявність тексту повідомлення у "alert: clicked: quit"
     @Test
     public void isAretsTextEquals(){
@@ -19,4 +19,4 @@ import org.openqa.selenium.chrome.ChromeDriver;
         Assertions.assertEquals("clicked: quit", alert.getText());
         webDriver.quit();
     }
-}*/
+}
