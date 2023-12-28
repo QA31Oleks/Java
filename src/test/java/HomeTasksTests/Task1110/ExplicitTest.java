8e21717a25ddb0f624f9fcb2ec1b7587ef66a747
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2023-12-28 09:44:05 +0200

    Changed waits

--- src/test/java/HomeTasksTests/Task1110/ExplicitTest.java
+++ src/test/java/HomeTasksTests/Task1110/ExplicitTest.java
@@ -1,13 +1,13 @@
 package HomeTasksTests.Task1110;
 
-//import org.example.Task1110.Explicit;
+import org.HomeTasks.Task1110.Explicit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-/*public class ExplicitTest {
+public class ExplicitTest {
     @Test
     public void checkingTextInExplicit(){
         Explicit explicit = new Explicit();
         Assertions.assertEquals("Бажаєте отримати набір\n" +
                 "демонстраційних уроків?", explicit.getTextOfAds());
     }
-}*/
+}
--- src/test/java/HomeTasksTests/Task1110/WaitsBlueTest.java
+++ src/test/java/HomeTasksTests/Task1110/WaitsBlueTest.java
@@ -1,13 +1,13 @@
 package HomeTasksTests.Task1110;
 
-//import org.example.Task1110.WaitsBlue;
+import org.HomeTasks.Task1110.WaitsBlue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-/*public class WaitsBlueTest {
+public class WaitsBlueTest {
     @Test
     public void checkingBlueInBody(){
         WaitsBlue waitsBlue = new WaitsBlue();
         Assertions.assertEquals("blue", waitsBlue.waitBlue());
     }
-}*/
+}
